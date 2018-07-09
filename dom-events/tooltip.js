f0627bd22e89b4386298d2d6e8e86418714f8224
Mikhail Lis <mixal8411@gmail.com>
2018-07-09 19:28:56 +0300

    refactor(dom-events): now everything works but elements without tips show blank tips

--- dom-events/tooltip.js
+++ dom-events/tooltip.js
@@ -6,8 +6,8 @@ class Tooltip {
     this.init();
   }
   init () {
-    this.element.addEventListener('mouseenter', () => this.showToolTip());
-    this.element.addEventListener('mouseleave', () => this.hideToolTip());
+    this.element.addEventListener('mouseenter', (e) => this.showToolTip(e));
+    this.element.addEventListener('mouseleave', (e) => this.hideToolTip(e));
   }
   createToolTipContainer () {
     const tooltip = this.element.getAttribute('data-tooltip');
@@ -16,19 +16,9 @@ class Tooltip {
     container.innerHTML = tooltip;
     return container;
   }
-  showToolTip () {
-    this.createToolTipContainer ();
-    const tooltip = this.createToolTipContainer();
-    document.body.appendChild(tooltip);
-
-    this.setCoords(container);
-    this.toolTipElem = container;
-  }
-
   setCoords(tipElem){
     const container = tipElem;
-    const target = this.element;
-    const coords = target.getBoundingClientRect();
+    const coords = this.element.getBoundingClientRect();
     let left = coords.left;
     if (left < 0) {
       left = 0;
@@ -36,11 +26,21 @@ class Tooltip {
 
     let top = coords.top - container.offsetHeight - 5;
     if (top < 0) {
-      top = coords.top + target.offsetHeight + 5;
+      top = coords.top + container.offsetHeight - 15;
     }
 
     container.style.left = left + 'px';
     container.style.top = top + 'px';
+  }
+  showToolTip () {
+    this.createToolTipContainer ();
+    const tooltip = this.createToolTipContainer();
+    document.body.appendChild(tooltip);
+
+    if (tooltip) {
+      this.setCoords(tooltip);
+      this.toolTipElem = tooltip;
+    }
   }
   hideToolTip () {
     if (this.toolTipElem) {
