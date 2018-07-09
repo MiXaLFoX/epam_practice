e45ff612b9935c207b4a2c23b5b4105c864994e8
Mikhail Lis <mixal8411@gmail.com>
2018-07-09 11:43:23 +0300

    refactor(dom-events): make Tooltip class

--- dom-events/tooltip.js
+++ dom-events/tooltip.js
@@ -1,33 +1,41 @@
 
-let toolTipElem;
-
-const showToolTip = (e) => {
-  const target = e.target;
-  const tooltip = target.getAttribute('data-tooltip');
-  const container = document.createElement('div');
-  container.className = 'tooltip';
-  container.innerHTML = tooltip || 'no tooltip for this element';
-  document.body.appendChild(container);
-
-  const coords = target.getBoundingClientRect();
-
-  let left = coords.left + (target.offsetWidth - container.offsetWidth) / 2;
-  if (left < 0) left = 0;
-
-  let top = coords.top - container.offsetHeight - 5;
-  if (top < 0) {
-    top = coords.top + target.offsetHeight + 5;
+class Tooltip {
+  constructor(element) {
+    this.element = element;
+    this.toolTipElem = null;
   }
+  init () {
+    this.element.addEventListener('mouseover', (e) => this.showToolTip());
+    this.element.addEventListener('mouseout', (e) => this.hideToolTip());
+  }
+  showToolTip (e) {
+    e = event || window.event;
+    const target = e.target;
+    const tooltip = target.getAttribute('data-tooltip');
+    const container = document.createElement('div');
+    container.className = 'tooltip';
+    container.innerHTML = tooltip;
+    document.body.appendChild(container);
 
-  container.style.left = left + 'px';
-  container.style.top = top + 'px';
+    const coords = target.getBoundingClientRect();
 
-  toolTipElem = container;
-};
+    let left = coords.left + (target.offsetWidth - container.offsetWidth) / 2;
+    if (left < 0) left = 0;
 
-const hideToolTip = (e) => {
-  document.body.removeChild(toolTipElem);
-};
+    let top = coords.top - container.offsetHeight - 5;
+    if (top < 0) {
+      top = coords.top + target.offsetHeight + 5;
+    }
 
-document.addEventListener('mouseover', showToolTip);
-document.addEventListener('mouseout', hideToolTip);
+    container.style.left = left + 'px';
+    container.style.top = top + 'px';
+
+    this.toolTipElem = container;
+  }
+  hideToolTip (e) {
+    document.body.removeChild(this.toolTipElem);
+  }
+}
+
+const tooltip = new Tooltip(document.querySelector('ul'));
+tooltip.init();
